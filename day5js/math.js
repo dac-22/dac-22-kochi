0ba1a9419bb74ef8546598a9d6b7dc808ce34336
santoshmondal <santosh.ece06@gmail.com>
2022-12-09 10:06:22 +0530

    js

---
+++ day5js/math.js
@@ -0,0 +1,12 @@
+function sum(n1, n2) {
+  return n1 + n2;
+}
+
+function mul(p1, p2) {
+  let output = p1 * p2;
+  return output;
+}
+
+// calling the function
+let output = sum(1, 1);
+console.log(output);
