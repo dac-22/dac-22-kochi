16eeb2e3068e769a9fae1d82972381a5faf984f6
santoshmondal <santosh.ece06@gmail.com>
2022-12-10 11:41:00 +0530

    cool.

--- day6/2.js
+++ day6/2.js
@@ -1,4 +1,4 @@
-function addNewMessageStatic() {
+function addNewMessage() {
   let divRef = document.querySelector("#message-container");
 
   // divRef.innerHTML += "<h1>hello </h1>";
@@ -9,10 +9,39 @@ function addNewMessageStatic() {
   divRef.innerHTML = newElement + divRef.innerHTML;
 }
 
+function addNewMessageV1() {
+  let divRef = document.querySelector("#message-container");
+  let textRef = document.querySelector("#text-id1");
+
+  let newElement = `<h1>${textRef.value}</h1>`;
+  divRef.innerHTML = newElement + divRef.innerHTML;
+}
+
+function addNewMessageV2() {
+  let divRef = document.querySelector("#message-container");
+  let textRef = document.querySelector("#text-id1");
+
+  let newElement = `<h1>${textRef.value}</h1>`;
+  divRef.innerHTML = newElement + divRef.innerHTML;
+
+  // make empty
+  textRef.value = "";
+}
+
 function addNewMessage() {
   let divRef = document.querySelector("#message-container");
   let textRef = document.querySelector("#text-id1");
 
   let newElement = `<h1>${textRef.value}</h1>`;
   divRef.innerHTML = newElement + divRef.innerHTML;
+
+  // make empty
+  textRef.value = "";
+}
+
+function checkEnter(event) {
+  // Enter has been pressed
+  if (event.keyCode == "13") {
+    addNewMessage();
+  }
 }
