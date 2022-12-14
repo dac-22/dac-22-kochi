f8c439add965a25988b39b8ad3b9e03b16b8ab50
santoshmondal <santosh.ece06@gmail.com>
2022-12-14 11:18:58 +0530

    basic messagin.

--- day10/1.js
+++ day10/1.js
@@ -1,6 +1,28 @@
-// ArrayList<String>
-// ArrayList<Person>
+function addMessage(reply) {
+  let parentRef = document.querySelector("#container");
 
-// JS
-let list = [1, 3, 4, 4];
-let list1 = ["a", "b", "c"];
+  let textRef = document.querySelector("#textId1");
+  let messageTime = new Date();
+  let hour = messageTime.getHours();
+  let minutes = messageTime.getMinutes();
+
+  let dclass = "justify-content-start";
+  if (reply) {
+    dclass = "justify-content-end";
+  }
+
+  let newMessage = `<div class="row mb-1 ${dclass}">
+                        <div class="col-6">
+                            <div
+                            class="d-flex badge text-bg-secondary justify-content-between align-items-center"
+                            >
+                            <div>${textRef.value}</div>
+                            <div style="font-size: 10px">${hour}:${minutes}</div>
+                            </div>
+                        </div>
+                    </div>`;
+
+  parentRef.innerHTML = newMessage + parentRef.innerHTML;
+
+  textRef.value = "";
+}
