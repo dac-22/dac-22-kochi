1034225383e3e234ee936bf60aab78ee2cc7a200
santoshmondal <santosh.ece06@gmail.com>
2022-12-19 12:21:37 +0530

    map.

--- day5js/19.js
+++ day5js/19.js
@@ -1,5 +1,6 @@
 // let outputList = ["Hello delhi", "Hello calcutta", "Hello kochin"];
 let inputList = ["delhi", "calcutta", "kochin"];
+// let outputList = ["<h1>delhi</h1>", "<h1>calcutta</h1>", "<h1>delhi</h1>"];
 
 // Conventional way of thinking.
 let outputList = [];
@@ -14,3 +15,6 @@ console.log(outputList);
 // Functional Way using Lamda/Arrow
 let outputList1 = inputList.map((item) => `Hello ${item}`);
 console.log(outputList1);
+
+let outputList2 = inputList.map((item) => `<h1>${item}</h1>`);
+console.log(outputList2);
